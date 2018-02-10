53ae8f6ebe2880c799de983b8380cc02dbff3a8f
DESKTOP-72Q1U3S\Sam <hebertsa07@gmail.com>
2018-02-09 19:27:42 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from  '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  section = "";
+  search = "";
+  sectionOptions = ["home","opinion", 
+    "world","national","politics","upshot","nyregion","business","technology","science","health","sports","arts","books","movies","theater","sundayreview","fashion","tmagazine","food","travel","magazine","realestate","automobiles","obituaries",  "insider"];
+
+  constructor(){
+
+  }
+
+  find(){
+    console.log(this.search)
+    console.log(this.section)
+  }
 }
+
+  // getSelection(){
+  //   return
+  // }
